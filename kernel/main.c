fec7255175169244e8f2774efc5c1241ca10a85f
Hugo Torres <hugo.torres1993@gmail.com>
2013-11-25 21:53:25 -0500

    Finished typing in the paging code but it's giving compile errors

--- kernel/main.c
+++ kernel/main.c
@@ -3,6 +3,7 @@
 #include "monitor.h"
 #include "descriptor_tables.h"
 #include "timer.h"
+#include "paging.h"
 
 int main(){
     // setup stack protector
@@ -24,7 +25,12 @@ int main(){
     asm volatile("int $0x4");
 
     asm volatile("sti");
-//    init_timer(1000);
-	PANIC("PANIC IS WORKING HOLY CRAP I'M SCARED!");
-    return 0;
+    monitor_clear();
+	initialize_paging();
+	monitor_write("Hello, paging world!\n");
+	
+	u32int* ptr = (u32int*) 0xA0000000;
+	u32int do_page_fault = *ptr;
+
+	return 0;
 }
--- kernel/paging.c
+++ kernel/paging.c
@@ -1,3 +1,5 @@
+#include "paging.h"
+
 /*
 * A bitset of frames - used or free
 */
@@ -28,7 +30,7 @@ static void set_frame(u32int frame_addr)
         u32int frame = frame_addr/0x1000;
         u32int idx = index_from_bit(frame);
         u32int off = offset_from_bit(frame);
-        frames[idx] |= (0x1 << off); //how can we index a pointer that has no address yet?
+        frames[idx] |= (0x1 << off);
 }
 /*
 * Clear a bit in the frames bitset
@@ -105,4 +107,92 @@ void free_frame(page_t* page)
                 return; //We never had a frame in the first place
         clear_frame(frame);
         page->frame = 0x0;
+}
+
+void initialize_paging()
+{
+	//The size of physical memory. For now let's just assume it's 16MB
+	u32int mem_end_page = 0x1000000;
+
+	frame_count = mem_end_page / 0x1000;
+	frames = (u32int*) kmalloc(index_from_bit(frame_count));
+	memset(frames, 0, index_from_bit(frame_count));
+
+	//Create a page directory
+	kernel_directory = (page_directory_t*) kmalloc_a(sizeof(page_directory_t));
+	memset(kernel_directory, 0, sizeof(page_directory_t);
+	current_directory = kernel_directory;
+	
+	/*
+	 * We need to identity map (phys addr = virt addr) from 0x0 to the end of
+	 * used memory, so we can access this transparently as if paging wasn't
+	 * enabled. NOTE that we use a while loop here deliberately. Inside the
+	 * loop body we actually change placement_address by calling kmalloc(). A
+	 * while loop causes this to be computed on-the-fly rather than once at
+	 * the start.
+	 */ 
+	int i = 0;
+	while(i < placement_address){
+		//Kernel code is readable but not writeable from userspace.
+		alloc_frame(get_page(i, 1, kernel_directory), 0, 0);
+		i += 0x1000;
+	}
+	register_interrupt_handler(14, page_fault);
+
+	switch_page_directory(kernel_directory);
+}
+
+void switch_page_directory(page_directory_t* dir)
+{
+	current_directory = dir;
+	asm volatile("mov %0, %%cr3":: "r"(&dir->tablesPhysical));
+	u32int cr0;
+	asm volatile("mov %%cr0, %0": "=r"(cr0));
+	cr0 |= 80000000;
+	asm volatile("mov %0, %%cr0":: "r"(cr0));
+}
+
+page_t* get_page(u32int address, int make, page_directory_t* dir)
+{
+	address /= 0x1000;
+	u32int table_idx = address / 1024;
+	if(dir->tables[table_idx]){
+		return &dir->tables[table_idx]->pages[address%1024];
+	}else if(make){
+		u32int tmp;
+		dir->tables[table_idx] = (page_table_t*)kmalloc_ap(sizeof(page_table_t), &tmp);
+		memset(dir->tables[table_idx], 0, 0x1000);
+		dir->tablesPhysical[table_idx] = tmp | 0x7; //PRESENT, RW, US.
+		return &dir->tables[table_idx]->pages[address%1024];
+	else{
+		return 0;
+	}
+	}
+}
+
+void page_fault(registers_t regs)
+{
+	u32int faulting_address
+	asm volatile("mov %%cr2, %0" : "=r" (faulting_address));
+
+	int present = !(regs.err_code & 0x1); //page not present
+	int rw = regs.err_code & 0x2; //Write operation?
+	int us = regs.err_code & 0x4; //Processor in user mode?
+	int reserved = regs.err_code & 0x8; //overwritten cpu reserved bits (entry)
+	int id = regs.err_code & 0x10; //caused by instruction fetch?
+
+	//Output error message
+	monitor_write("Page fault! ( ");
+	if(present)
+		monitor_write("missing ");
+	if(rw)
+		monitor_write("read-only ");
+	if(us)
+		monitor_write("user-mode ");
+	if(reserved)
+		monitor_write("reserved ");
+	monitor_write(") at 0x");
+	monitor_write_hex(faulting_address);
+	monitor_write("\n");
+	PANIC("Page fault");
 }
